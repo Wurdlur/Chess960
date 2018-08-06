b9485d05f5516308ff65715386dfe0a02ba7ba96
Ginger John <JAuchterlonie@student.neumont.edu>
2018-08-06 16:53:02 -0600

    Final Chess Picked

---
+++ app.js
@@ -0,0 +1,78 @@
+var game = new Chess();
+var board = new ChessBoard('board', {
+  onSquareClick: onSquareClick
+});
+
+function onSquareClick(clickedSquare, selectedSquares) {
+  if (selectedSquares.length === 0) {
+    if (game.moves({ square: clickedSquare }).length > 0) {
+      board.selectSquare(clickedSquare);
+    }
+
+    return;
+  }
+
+  var selectedSquare = selectedSquares[0];
+   
+  if (clickedSquare === selectedSquare) {
+    board.unselectSquare(clickedSquare);
+    return;
+  }
+
+  board.unselectSquare(selectedSquare);
+
+  var clickedPieceObject = game.get(clickedSquare);
+  var selectedPieceObject = game.get(selectedSquare);
+
+  if (clickedPieceObject && (clickedPieceObject.color === selectedPieceObject.color)) {
+    board.selectSquare(clickedSquare);
+    return;
+  }
+
+  var legalMoves = game.moves({ square: selectedSquare, verbose: true });
+  var isMoveLegal = legalMoves.filter(function(move) {
+    return move.to === clickedSquare;
+  }).length > 0;
+
+  if (!isMoveLegal) {
+    return;
+  }
+
+  if (selectedPieceObject.type === 'p' && (clickedSquare[1] === '1' || clickedSquare[1] === '8')) { // Promotion
+    board.askPromotion(selectedPieceObject.color, function(shortPiece) {
+      move(selectedSquare, clickedSquare, shortPiece);
+    });
+  } else {
+    move(selectedSquare, clickedSquare);
+  }
+}
+
+function move(from, to, promotionShortPiece) {
+  game.move({
+    from: from,
+    to: to,
+    promotion: promotionShortPiece
+  });
+
+  board.setPosition(game.fen());
+
+  randomMove();
+}
+
+function randomMove() {
+  var legalMoves = game.moves();
+
+  var randomIndex = Math.floor(Math.random() * legalMoves.length);
+
+  game.move(legalMoves[randomIndex]);
+  board.setPosition(game.fen());
+
+  if (game.game_over()) {
+    if (game.in_checkmate()) {
+      alert('You ' + (game.turn() === 'w' ? 'lost' : 'won'));
+    } else {
+      alert('It\'s a draw');
+    }
+  }
+}
+
